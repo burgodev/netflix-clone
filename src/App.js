d586a6f0954929df037af49ac7eba9f757e7d8c8
filipe.burgonovo <filipe.burgonovo@ecoportal.co.nz>
2024-01-30 20:29:32 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./HomeScreen";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // TODO: Stopped at 1:28:46
 
 function App() {
   return (
     <div className="app">
-      {/* <header className="App-header"> */}
-      <HomeScreen />
-      {/* </header> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen />}></Route>
+          <Route path="/test" element={<h1>teste</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
