f2af369ac9533491783d572a254c0ecde57399cd
filipe.burgonovo <filipe.burgonovo@ecoportal.co.nz>
2024-01-30 20:09:47 -0300

    Row & FireBase Deploy

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import "./App.css";
 import HomeScreen from "./HomeScreen";
 
+// TODO: Stopped at 1:28:46
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       {/* <header className="App-header"> */}
       <HomeScreen />
       {/* </header> */}
