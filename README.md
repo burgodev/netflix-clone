89516e2ecb8fb7fafe2a1c7ac89c16f7426e00e3
filipe.burgonovo <filipe.burgonovo@ecoportal.co.nz>
2024-02-23 19:40:20 -0300

    Final Setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # netflix-clone
+Based on Sonny Sangha work. https://www.youtube.com/watch?v=HW5roUF2RLg&ab_channel=SonnySangha
+
+# Start App
+npm start
+
+# Build App
+npm run build
+
+# Deploy App
+firebase deploy
+
--- src/screens/ProfileScreen/components/Plans/Plans.js
+++ src/screens/ProfileScreen/components/Plans/Plans.js
@@ -10,9 +10,27 @@ const stripePublishableKey =
   "pk_test_51OjnkYGcuMp15AOG7nnQM0zCoHm1SQB7axweBtlusdXu46Ad6m0qqvbGUqkKnZoXJlrJiJaNmsPFaGsJZeDteOXZ00azXe1os2";
 
 const Plans = () => {
+  const user = useSelector(selectUser);
   const [products, setProducts] = useState([]);
   const [isLoadingCheckout, setIsLoadingCheckout] = useState(false);
-  const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    db.collection("customers")
+      .doc(user.uid)
+      .collection("subscriptions")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+          });
+        });
+      });
+  }, [user.uid]);
 
   useEffect(() => {
     db.collection("products")
@@ -63,23 +81,39 @@ const Plans = () => {
     });
   };
 
-  console.log("products", products);
-  console.log("Object.entries(products)", Object.entries(products));
-
   return (
     <div>
+      <br />
+      {subscription && (
+        <p>
+          Renewal date:{" "}
+          {new Date(subscription?.current_period_end * 1000).toLocaleString()}
+        </p>
+      )}
+
       {Object.entries(products).map(([productId, productData]) => {
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
+
         return (
-          <div key={productId} className="plansContainer">
+          <div
+            key={productId}
+            className={`${
+              isCurrentPackage && "planScreenDisabled"
+            } plansContainer`}
+          >
             <div className="plansInfo">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
             <button
-              onClick={() => loadCheckout(productData.prices.priceId)}
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
               disabled={isLoadingCheckout}
             >
-              {isLoadingCheckout ? "Loading..." : "Subscribe"}
+              {isCurrentPackage ? "Current Package" : "Subscribe"}
             </button>
           </div>
         );
